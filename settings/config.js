abefb11a6b5d5cec4320053bf44795c8a6fa7e3a
kakwizard <80742169+kakwizard@users.noreply.github.com>
2022-08-13 13:07:12 +0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { resolve } = require("path");
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "Nzk4NDI2MzQwNzM0MjcxNDg4.GFfp5J.HlGdAMhwQlvor4EhIbzOVy-NGnbqv87AZhPnOI",  // your bot token
+    TOKEN: process.env.TOKEN || "OTg2OTE5MjU1NTMyNTExMzAy.GAZ72P.Zl0a0RxivSpN5VcxgwH_DGppi9hII-tlUj65VQ",  // your bot token
     PREFIX: process.env.PREFIX || "#", //<= default is #  // bot prefix
     EMBED_COLOR: process.env.EMBED_COLOR || "#000001", //<= default is "#000001"
 
